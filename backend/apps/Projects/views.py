46af1187cccd52f12fb743712c2850bea789e2ac
Sebastian <se6359@gmail.com>
2022-10-04 22:54:34 +0200

    images

--- backend/apps/Projects/views.py
+++ backend/apps/Projects/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from .models import Project
 from .serializers import ProjectSerializer
 from rest_framework import generics
@@ -5,3 +6,10 @@ from rest_framework import generics
 class ProjectView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+def ImagesUrls(request, id):
+    obj = Project.objects.get(pk=id)
+    context= {
+        'object': obj
+    }
+    return render(request, 'dist/index.html', context)
