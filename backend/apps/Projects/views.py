067430a81f5d93ffc47b0eccb37d7f52a4f3ad15
Sebastian <se6359@gmail.com>
2022-10-04 21:41:36 +0200

    changed generics

--- backend/apps/Projects/views.py
+++ backend/apps/Projects/views.py
@@ -2,6 +2,6 @@ from .models import Project
 from .serializers import ProjectSerializer
 from rest_framework import generics
 
-class ProjectView(generics.RetrieveAPIView):
+class ProjectView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
