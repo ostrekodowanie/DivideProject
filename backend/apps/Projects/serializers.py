cb26c7eb43b6e465572e33d757575d2af995e460
Sebastian <se6359@gmail.com>
2022-10-04 23:42:15 +0200

    changed technologies array

--- backend/apps/Projects/serializers.py
+++ backend/apps/Projects/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    technologies = serializers.StringRelatedField(many=True, read_only=True)
     class Meta:
         model = Project
         fields = '__all__'
