cf61be4e43171ce2c6d86f6a4074ea0a1618fa2a
Filip <filfer05@gmail.com>
2022-10-06 22:55:16 +0200

    placeholder fix

--- frontend/src/components/contact/Budget.jsx
+++ frontend/src/components/contact/Budget.jsx
@@ -25,7 +25,7 @@ export default function Budget({ setInfo }) {
     return (
         <div className={`relative z-10 flex flex-col justify-center ${inputStyles}`}>
             <label htmlFor='budget' className="z-10 absolute inset-0 flex items-center">
-                <span className='transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left -translate-y-[60%] scale-75'>Goal</span>
+                <span className='transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left -translate-y-[60%] scale-75'>Budget</span>
             </label>
             <input className='bg-transparent select-none outline-none cursor-pointer font-medium transition duration-300 relative z-20 translate-y-[30%]' readOnly required autoComplete="off" onClick={() => setActive(prev => !prev)} type="text" id='budget' value={input} />
             {active && <div className="absolute top-full right-0 left-0 rounded-xl overflow-hidden">
