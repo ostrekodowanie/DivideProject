2f4512538c379da9d88e209a7d07dd657724fccf
Filip <filfer05@gmail.com>
2022-10-06 22:51:26 +0200

    fix

--- frontend/src/components/contact/Budget.jsx
+++ frontend/src/components/contact/Budget.jsx
@@ -25,9 +25,9 @@ export default function Budget({ setInfo }) {
     return (
         <div className={`relative z-10 flex flex-col justify-center ${inputStyles}`}>
             <label htmlFor='budget' className="z-10 absolute inset-0 flex items-center">
-                <span className={`transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left ${active ? '-translate-y-[60%] scale-75' : ''}`}>Goal</span>
+                <span className='transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left -translate-y-[60%] scale-75'>Goal</span>
             </label>
-            <input className={`bg-transparent select-none outline-none cursor-pointer font-medium transition duration-300 relative z-20 ${active ? 'translate-y-[30%]' : ''}`} readOnly required autoComplete="off" onClick={() => setActive(prev => !prev)} type="text" id='budget' value={input} />
+            <input className='bg-transparent select-none outline-none cursor-pointer font-medium transition duration-300 relative z-20 translate-y-[30%]' readOnly required autoComplete="off" onClick={() => setActive(prev => !prev)} type="text" id='budget' value={input} />
             {active && <div className="absolute top-full right-0 left-0 rounded-xl overflow-hidden">
                 <ul className="flex flex-col">
                     {options.map(option => <li onClick={() => setInput(option)} className="bg-background cursor-pointer text-font font-medium py-3 px-5 flex items-center justify-between" key={option}>{option}{input === option && <span className="bg-primary bg-clip-text text-transparent font-bold">X</span>}</li>)}
--- frontend/src/components/contact/Goal.jsx
+++ frontend/src/components/contact/Goal.jsx
@@ -26,9 +26,9 @@ export default function Goal({ setInfo }) {
     return (
         <div className={`relative flex flex-col z-10 justify-center ${inputStyles}`}>
             <label htmlFor='goal' className="z-10 select-none absolute inset-0 flex items-center">
-                <span className={`transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left ${active ? '-translate-y-[60%] scale-75' : ''}`}>Goal</span>
+                <span className='transition left-6 absolute font-medium text-[#4C3963] duration-300 origin-left -translate-y-[60%] scale-75'>Goal</span>
             </label>
-            <input className={`bg-transparent select-none outline-none cursor-pointer font-medium transition duration-300 relative z-20 ${active ? 'translate-y-[30%]' : ''}`} readOnly required autoComplete="off" onClick={() => setActive(prev => !prev)} type="text" id='goal' value={input} />
+            <input className='bg-transparent select-none outline-none cursor-pointer font-medium transition duration-300 relative z-20 translate-y-[30%]' readOnly required autoComplete="off" onClick={() => setActive(prev => !prev)} type="text" id='goal' value={input} />
             {active && <div className="absolute top-full right-0 left-0 rounded-xl overflow-hidden">
                 <ul className="flex flex-col">
                     {options.map(option => <li onClick={() => setInput(option)} className="bg-background cursor-pointer text-font font-medium py-3 px-5 flex items-center justify-between" key={option}>{option}{input === option && <span className="bg-primary bg-clip-text font-bold text-transparent">X</span>}</li>)}
