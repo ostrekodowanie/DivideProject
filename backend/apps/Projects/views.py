de39765f53b71f1f278e5719d5b2214434ac7daf
Sebastian <se6359@gmail.com>
2022-10-20 22:03:59 +0200

    backend

--- backend/apps/Projects/views.py
+++ backend/apps/Projects/views.py
@@ -6,10 +6,3 @@ from rest_framework import generics
 class ProjectView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
-def ImagesUrls(request, id):
-    obj = Project.objects.get(pk=id)
-    context= {
-        'object': obj
-    }
-    return render(request, 'dist/index.html', context)
