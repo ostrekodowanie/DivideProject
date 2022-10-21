287357bc82fd9ab69af681f2ebb983d1451c4e56
Filip <filfer05@gmail.com>
2022-10-21 19:17:51 +0200

    cover slider

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -57,24 +57,40 @@ const gridItems = [
     fourth
 ]
 
-const Grid = () => {
+const CoverSlider = ({ activeImage, setSliderImage }) => {
+    const [active, setActive] = useState(activeImage)
     return (
-        <div className="flex flex-col gap-8">
-            <div className="flex flex-col gap-4">
-                <h3 className="bg-primary bg-clip-text text-transparent font-medium tracking-wider text-lg">DESIGN PRESENTATION</h3>
-                <h2 className="text-font text-4xl lg:text-5xl font-medium">Layouts</h2>
-            </div>
-            <div className="flex flex-col sm:grid grid-cols-2 gap-8">
-                {gridItems.map((project, i) => <GridItem src={project} size={i % 2 === 0 && i !== 0 ? 'wide' : 'narrow'} key={project} />)}
+        <div className="fixed backdrop-blur-md z-50 bg-pros inset-0 hidden md:flex items-center justify-center">
+            <div onClick={() => setSliderImage(null)} className="absolute cursor-pointer bg-blur text-white font-bold h-12 w-12 right-12 top-12 rounded-full flex items-center justify-center">X</div>
+            <img className="h-[60vh] w-[40vw] mb-16 rounded-xl object-cover" src={active} alt="" />
+            <div className="absolute bottom-12 flex items-center gap-4">
+                {gridItems.map((project, i) => i % 2 !== 0 || i === 0 ? <img onClick={() => setActive(project)} className="max-h-[1in] cursor-pointer rounded hover:scale-105 transition-transform" src={project} key={project} /> : <></>)}
             </div>
         </div>
     )
 }
 
-const GridItem = ({ src, size }) => {
+const Grid = () => {
+    const [sliderImage, setSliderImage] = useState(null)
     return (
-        <div className={`rounded-xl gridItem bg-pros backdrop-blur-md p-6 ${size === 'wide' ? 'col-span-2' : ''}`}>
+        <div className="flex flex-col gap-8">
+            {sliderImage && <CoverSlider activeImage={sliderImage} setSliderImage={setSliderImage} />}
+            <div className="flex flex-col gap-4">
+                <h3 className="bg-primary bg-clip-text text-transparent font-medium tracking-wider text-lg">DESIGN PRESENTATION</h3>
+                <h2 className="text-font text-4xl lg:text-5xl font-medium">Layouts</h2>
+            </div>
+            <div className="flex flex-col sm:grid grid-cols-2 gap-8">
+                {gridItems.map((project, i) => <GridItem setSliderImage={setSliderImage} src={project} size={i % 2 === 0 && i !== 0 ? 'wide' : 'narrow'} key={project} />)}
+            </div>
+        </div>
+    )
+}
+
+const GridItem = ({ src, size, setSliderImage }) => {
+    return (
+        <div onClick={() => size === 'narrow' && setSliderImage(src)} className={`rounded-xl gridItem bg-pros backdrop-blur-md p-6 ${size === 'wide' ? 'col-span-2' : 'hover:scale-[1.02] cursor-pointer transition-transform duration-300'}`}>
             <img className={`object-cover rounded-xl w-full ${size === 'narrow' && 'max-h-[4.2in]'}`} src={src} alt="" />
         </div>
     )
 }
+
